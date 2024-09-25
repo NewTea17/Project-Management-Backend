f6d09069a5a55ec8427becd7b693d950e86b8e0d
Yaroslav Guz <aroslavguz@gmail.com>
2024-09-25 17:49:58 +0300

    Change UserService and create user service impl

--- src/main/java/com/lpnu/projectmanagementsystem/services/UserService.java
+++ src/main/java/com/lpnu/projectmanagementsystem/services/UserService.java
@@ -1,73 +1,16 @@
 package com.lpnu.projectmanagementsystem.services;
 
-import com.lpnu.projectmanagementsystem.config.JwtProvider;
 import com.lpnu.projectmanagementsystem.entities.UserEntity;
-import com.lpnu.projectmanagementsystem.repositories.UserRepository;
 import com.lpnu.projectmanagementsystem.requests.LoginRequest;
 import com.lpnu.projectmanagementsystem.responses.AuthResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
-    @Autowired
-    private UserRepository userRepository;
+public interface UserService {
+    AuthResponse createUser(UserEntity user) throws Exception;
+    AuthResponse login(LoginRequest request);
 
-    @Autowired
-    private CustomUserDetailsServiceImpl userDetailsService;
+    UserEntity findUserProfileByJwt(String jwt) throws Exception;
+    UserEntity findUserByEmail(String email) throws Exception;
+    UserEntity findUserById(Long id) throws Exception;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    public AuthResponse createUser(UserEntity user) throws Exception {
-        UserEntity candidate = userRepository.findByEmail(user.getEmail());
-        if (candidate != null) {
-            throw new Exception(String.format("Email '%s' already exists", user.getEmail()));
-        }
-
-        UserEntity createdUser = new UserEntity();
-        createdUser.setFullName(user.getFullName());
-        createdUser.setEmail(user.getEmail());
-        createdUser.setPassword(passwordEncoder.encode(user.getPassword()));
-
-        Authentication authentication = new UsernamePasswordAuthenticationToken(createdUser.getEmail(), createdUser.getPassword());
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwt = JwtProvider.generateToken(authentication);
-
-        AuthResponse res = new AuthResponse(jwt, "Sign up successfully");
-
-        userRepository.save(createdUser);
-
-        return res;
-    }
-
-    public AuthResponse login(LoginRequest request) {
-        String username = request.getEmail();
-        String password = request.getPassword();
-
-        Authentication authentication = authenticate(username, password);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwt = JwtProvider.generateToken(authentication);
-
-        return new AuthResponse(jwt, "Sign in successfully");
-    }
-
-    private Authentication authenticate(String username, String password) {
-        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-        if (userDetails == null) {
-            throw new BadCredentialsException("Invalid username!");
-        }
-
-        if (!passwordEncoder.matches(password, userDetails.getPassword())) {
-            throw new BadCredentialsException("Invalid password!");
-        }
-
-        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-    }
+    UserEntity updateUserCountOfProjects(UserEntity user, int num);
 }
---
+++ src/main/java/com/lpnu/projectmanagementsystem/services/impl/UserServiceImpl.java
@@ -0,0 +1,111 @@
+package com.lpnu.projectmanagementsystem.services.impl;
+
+import com.lpnu.projectmanagementsystem.config.JwtProvider;
+import com.lpnu.projectmanagementsystem.entities.UserEntity;
+import com.lpnu.projectmanagementsystem.repositories.UserRepository;
+import com.lpnu.projectmanagementsystem.requests.LoginRequest;
+import com.lpnu.projectmanagementsystem.responses.AuthResponse;
+import com.lpnu.projectmanagementsystem.services.CustomUserDetailsServiceImpl;
+import com.lpnu.projectmanagementsystem.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private CustomUserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public AuthResponse createUser(UserEntity user) throws Exception {
+        UserEntity candidate = userRepository.findByEmail(user.getEmail());
+        if (candidate != null) {
+            throw new Exception(String.format("Email '%s' already exists", user.getEmail()));
+        }
+
+        UserEntity createdUser = new UserEntity();
+        createdUser.setFullName(user.getFullName());
+        createdUser.setEmail(user.getEmail());
+        createdUser.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(createdUser.getEmail(), createdUser.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = JwtProvider.generateToken(authentication);
+
+        AuthResponse res = new AuthResponse(jwt, "Sign up successfully");
+
+        userRepository.save(createdUser);
+
+        return res;
+    }
+
+    @Override
+    public AuthResponse login(LoginRequest request) {
+        String username = request.getEmail();
+        String password = request.getPassword();
+
+        Authentication authentication = authenticate(username, password);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = JwtProvider.generateToken(authentication);
+
+        return new AuthResponse(jwt, "Sign in successfully");
+    }
+
+    @Override
+    public UserEntity findUserProfileByJwt(String jwt) throws Exception {
+        String email = JwtProvider.extractEmail(jwt);
+        return findUserByEmail(email);
+    }
+
+    @Override
+    public UserEntity findUserByEmail(String email) throws Exception {
+        UserEntity user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new Exception("User not found");
+        }
+
+        return user;
+    }
+
+    @Override
+    public UserEntity findUserById(Long id) throws Exception {
+        Optional<UserEntity> optionalUser = userRepository.findById(id);
+        if (optionalUser.isEmpty()) {
+            throw new Exception("User not found");
+        }
+
+        return optionalUser.get();
+    }
+
+    @Override
+    public UserEntity updateUserCountOfProjects(UserEntity user, int num) {
+        user.setCountOfProjects(user.getCountOfProjects() + num);
+        return userRepository.save(user);
+    }
+
+    private Authentication authenticate(String username, String password) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        if (userDetails == null) {
+            throw new BadCredentialsException("Invalid username!");
+        }
+
+        if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+            throw new BadCredentialsException("Invalid password!");
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
+}
